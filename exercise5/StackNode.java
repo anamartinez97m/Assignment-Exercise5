8330097a3df2404fb75f9f1c9ab27e53917d58cc
AnaMartinez <anamartinez97m@gmail.com>
2018-09-03 15:10:34 +0200

    first commit

---
+++ exercise5/StackNode.java
@@ -0,0 +1,12 @@
+package assignment.exercise5;
+
+public class StackNode {
+    int elem;
+    StackNode next;
+    
+    public StackNode(int elem, StackNode next) {
+        this.elem = elem;
+        this.next = next;
+    }
+
+}
